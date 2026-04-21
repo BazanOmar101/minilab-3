98f9d467a63d2660adf3da824f5fbfab7085f96c
Omar Bazan <92182344+BazanOmar101@users.noreply.github.com>
2026-04-20 19:29:47 -0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "myclass.h"
+
+using namespace std;
+
+int main () {
+  cout << "Hello World" << endl;
+  cout << "New Line" << endl;
+  MyClass a(5);
+  cout << a.getX() << endl;
+
+}
+           
